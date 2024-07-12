af7f8bb897c90996bc840395d851a1af9ffdc175
dot-richard <dot-richard@users.noreply.github.com>
2024-07-12 17:24:56 +0200

    change: dircolors theme

--- bash/bashrc.d/dircolors.sh
+++ bash/bashrc.d/dircolors.sh
@@ -1,5 +1,5 @@
 # Le theme dircolors (pour la coloration des sorties)
-BASH_DIRCOLORS=~/.dircolors_themes/monokai
+BASH_DIRCOLORS=~/.dircolors_themes/monokai.dircolors
 
 if [ -f "$BASH_DIRCOLORS" ]; then
     eval $(dircolors "$BASH_DIRCOLORS")
