d7c3a2cf6c0cbc0045b5b90ffc41b31ce9937414
dot-richard <dot-richard@users.noreply.github.com>
2024-07-13 08:48:13 +0200

    fix: tester la commande avant execution

--- bash/bashrc.d/functions.sh
+++ bash/bashrc.d/functions.sh
@@ -14,7 +14,13 @@ function command_not_found_handle {
     # Désactiver la sensibilité a la casse.
     command=$(echo "$1" | tr '[:upper:]' '[:lower:]')
     shift
-    "$command" "$@"
+
+    # Vérifier si la commande existe dans le PATH
+    if command -v "$command" > /dev/null 2>&1; then
+        "$command" "$@"
+    else
+        echo "Commande non trouvée: '$command'."
+    fi
 }
 
 ##
