06ec3cea1373c4c3f6fc77806d47dd79f01ac49e
dot-richard <dot-richard@users.noreply.github.com>
2024-07-24 12:29:41 +0200

    better bash aliases

--- bash/bashrc.d/aliases.sh
+++ bash/bashrc.d/aliases.sh
@@ -1,6 +1,6 @@
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias config='cd ~/.bashrc.d'
 alias src='source ~/.bash_profile'
 alias bashrc="$EDITOR ~/.bashrc"
 alias bash_profile="$EDITOR ~/.bash_profile"
+alias bashconfig='cd ~/.bashrc.d'
